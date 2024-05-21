e3bd3e8bfaff9c77feb67259363aec752d82ea58
James4518 <james4518@foxmail.com>
2024-05-22 05:43:45 +0800

    bs4

--- kaola.py
+++ kaola.py
@@ -2,8 +2,10 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support import expected_conditions as EC
+from bs4 import BeautifulSoup
+import lxml
 
 class NavItem:
   def __init__(self, title, icons, categories_title, brands, more_brand):
@@ -37,57 +39,55 @@ class Kaola:
     })
     self.driver.get("https://www.kaola.com/")
 
+  def setup_bs(self, page_source):
+    self.soup = BeautifulSoup(page_source, 'lxml')
+
   def scrapy_nav(self):
-    nav = WebDriverWait(self.driver, 10).until(
-      EC.visibility_of_element_located((By.XPATH, '//ul[@class="catitmlst j-catmenu f-switch-catitems"]'))
-    )
+    nav = self.driver.find_element(By.XPATH, '//ul[@class="catitmlst j-catmenu f-switch-catitems"]')
     liList = nav.find_elements(By.XPATH, ".//li")
     for li in liList:
       title_element = li.find_element(By.XPATH, ".//span[@class='t']")
       all_category = title_element.text
       ActionChains(self.driver).move_to_element(title_element).perform()
-      action = ActionChains(self.driver)
-      action.move_to_element(title_element).perform()
       time.sleep(0.5)
 
-      icon_elements = li.find_elements(By.XPATH, ".//img[contains(@class, 'icon') or contains(@class, 'iconhv')]")
-      icons = []
-      for icon in icon_elements:
-        icons.append({
-          "src": icon.get_attribute("src"),
-          "srcset": icon.get_attribute("srcset")
-        })
+  def parse_nav(self):
+    ul = self.soup.find('ul', class_='catitmlst j-catmenu f-switch-catitems')
+    liList = ul.find_all('li')
+    for li in liList:
+      title_element = li.find('span', class_='t')
+      all_category = title_element.get_text()
 
-      big_category_contents = li.find_element(By.XPATH, ".//div[@class='m-ctgcard f-cb j-category_card']")
-      brandList = big_category_contents.find_elements(By.XPATH, ".//div[contains(@class,'m-brandbox')]")
+      icon_elements = li.find_all('img', class_='icon')
+      icons = [{"src": icon['src'], "srcset": icon['srcset']} for icon in icon_elements]
+
+      big_category_contents = li.find('div', class_='m-ctgcard f-cb j-category_card')
+      brandList = big_category_contents.find_all('div', class_='m-brandbox')
       brands = []
       for brand in brandList:
-        brandElements = brand.find_elements(By.XPATH, ".//a")
+        brandElements = brand.find_all('a')
         for brandElement in brandElements:
-            brandHref = brandElement.get_attribute("href")
-            picUrl = brandElement.find_element(By.XPATH, ".//img").get_attribute("src")
-            brands.append({
-              "href": brandHref,
-              "picUrl": picUrl
-            })
-      more_brand_element = big_category_contents.find_element(By.XPATH, ".//div[contains(@class,'imgbox')]")
-      more_brand_link = more_brand_element.find_element(By.XPATH, ".//a").get_attribute("href")
-      more_brand_picUrl = more_brand_element.find_element(By.XPATH, ".//img").get_attribute("src")
+          brandHref = brandElement['href']
+          picUrl = brandElement.find('img')['src']
+          brands.append({"href": brandHref, "picUrl": picUrl})
+
+      more_brand_element = big_category_contents.find('div', class_='imgbox')
+      more_brand_link = more_brand_element.find('a')['href']
+      more_brand_picUrl = more_brand_element.find('img')['src']
       more_brand = MoreBrand(more_brand_link, more_brand_picUrl)
 
-      litds = big_category_contents.find_elements(By.XPATH,"./div/div/div[@class='f-cb']/div[@class='litd']/div[@class='item']")
-      litd_titles = []
+      litds = big_category_contents.select('.litd .item')
+      litd_titles = [litd.find('p').find('a', class_=lambda x: x and x.startswith('cat2')).get_text() for litd in litds]
       categories_list = []
       category_dict = {}
-      for litd in litds: 
-        litd_title = litd.find_element(By.XPATH, ".//p/a[starts-with(@class, 'cat2')]").text
-        litd_titles.append(litd_title)
-        ctgbox = litd.find_element(By.XPATH, ".//div[@class='ctgnamebox']")
-        sub_categories = ctgbox.find_elements(By.XPATH, ".//a[starts-with(@class,'f-fcred')]")
-        category_dict[litd_title] = [sub_category.text for sub_category in sub_categories]
+      for litd in litds:
+        litd_title = litd.find('p').find('a', class_=lambda x: x and x.startswith('cat2')).get_text()
+        ctgbox = litd.find('div', class_='ctgnamebox')
+        sub_categories = ctgbox.find_all('a', class_=lambda x: x and x.startswith('f-fcred'))
+        category_dict[litd_title] = [sub_category.get_text() for sub_category in sub_categories]
         categories_list.append(litd_title)
-      self.categories_dict[all_category] = category_dict
 
+      self.categories_dict[all_category] = category_dict
       nav_item = NavItem(all_category, icons, litd_titles, brands, more_brand)
       self.navList.append(nav_item)
 
@@ -113,6 +113,9 @@ def main():
   kaola = Kaola()
   kaola.setup_driver()
   kaola.scrapy_nav()
+  page_source = kaola.driver.page_source
+  kaola.setup_bs(page_source)
+  kaola.parse_nav()
   kaola.close_driver()
   kaola.print_nav()
 
